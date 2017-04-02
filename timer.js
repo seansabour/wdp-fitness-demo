bdfa4352407b7cf849e95a6b0c464921c92e7160
Paul Portela <paulportela@Pauls-MacBook-Pro.local>
2017-04-02 10:41:36 -0700

    change time

--- timer.js
+++ timer.js
@@ -2,5 +2,5 @@ var uaclient = require('./ua-client');
 
 exports.timer = function() {
 	console.log("Interval is set to get aggregate data of users")
-    setInterval(uaclient.getAggregates, (30000*1));
+    setInterval(uaclient.getAggregates, (60000*30));
 }
